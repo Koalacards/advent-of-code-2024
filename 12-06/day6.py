979505295b50a09cd8685df1cd4c12b893635ad2
Hayden <44925247+Koalacards@users.noreply.github.com>
2024-12-08 18:54:33 -0500

    day 6 part 2
    ugh that took so dang long i forgot about the edge cases where it goes into a corner with obstacles to the front and right

--- 12-06/day6.py
+++ 12-06/day6.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from typing import List, Tuple, Optional
-from copy import deepcopy
 
 def parse_file(fp: Path) -> Tuple[List[List[str]], Tuple[int, int]]:
     grid =[]
@@ -47,12 +46,16 @@ def check_next_spot(grid: List[List[str]], pos: Tuple[int, int], direction: str)
         actual_next_pos = potential_next_pos
         new_direction = direction
     else:
+
         new_direction = direction_change_grid[direction]
         actual_next_pos = get_next_pos_at_direction(pos, new_direction)
-    
+        if grid[actual_next_pos[0]][actual_next_pos[1]] == "#":
+            new_direction = direction_change_grid[new_direction]
+            actual_next_pos = get_next_pos_at_direction(pos, new_direction)
+
     return (actual_next_pos, new_direction)
 
-def partOne(fp: Path) -> int:
+def partOne(fp: Path) -> List[Tuple[int, int]]:
     grid, starting_pos = parse_file(fp)
     live_pos = starting_pos
     live_direction = "up"
@@ -65,43 +68,48 @@ def partOne(fp: Path) -> int:
             live_pos, live_direction = next_spot_return[0], next_spot_return[1]
             if live_pos not in visited_pos:
                 visited_pos.append(live_pos)
-    return len(visited_pos)
+    return visited_pos
 
 
 def partTwo(fp: Path) -> int:
     count = 0
     grid, starting_pos = parse_file(fp)
-    for i, line in enumerate(grid):
-        for j in range(len(line)):
-            grid_copy = deepcopy(grid)
-            if grid_copy[i][j] != "#":
-                grid_copy[i][j] = "#"
+    visited_spots = partOne(fp)
+    visited_spots.remove(starting_pos)
+    for i, j in visited_spots:
+        if grid[i][j] == "#":
+            print(f"skipping ({i},{j})")
+            continue
+        
+        grid[i][j] = "#"
+        
+        live_pos = starting_pos
+        live_direction = "up"
+        visited_directory = {
+            live_direction: [live_pos]
+        }
+        while live_pos is not None:
+            next_spot_return = check_next_spot(grid, live_pos, live_direction)
+            
+            if next_spot_return is None:
+                if i == 6 and j == 77:
+                    raise ValueError("something still up")
+                live_pos = None
+                print(f"no loop at ({i},{j})")
             else:
-                print(f"skipping ({i},{j})")
-                continue
-            live_pos = starting_pos
-            live_direction = "up"
-            visited_directory = {
-                live_direction: [live_pos]
-            }
-            while live_pos is not None:
-                next_spot_return = check_next_spot(grid_copy, live_pos, live_direction)
-                if next_spot_return is None:
+                live_pos, live_direction = next_spot_return[0], next_spot_return[1]
+                if live_pos in visited_directory.get(live_direction, []):
+                    print(f"found loop with ({i},{j})")
+                    count += 1
                     live_pos = None
-                    print(f"no loop at ({i},{j})")
-                else:
-                    live_pos, live_direction = next_spot_return[0], next_spot_return[1]
-                    if live_pos in visited_directory.get(live_direction, []):
-                        print(f"found loop with ({i},{j})")
-                        count += 1
-                        live_pos = None
-                    dir_entry = visited_directory.get(live_direction, [])
-                    dir_entry.append(live_pos)
-                    visited_directory[live_direction] = dir_entry
+                dir_entry = visited_directory.get(live_direction, [])
+                dir_entry.append(live_pos)
+                visited_directory[live_direction] = dir_entry
+        grid[i][j] = "."
     return count
 
 
 fp = Path(__file__).parent / "data.txt"
 
-print(partOne(fp))
+print(len(partOne(fp)))
 print(partTwo(fp))
